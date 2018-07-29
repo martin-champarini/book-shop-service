7aebc763818c2704236602999f00dd9ad81b3503
Unknown <martin.champarini.dev@gmail.com>
2018-07-29 12:31:38 -0300

    First commit

---
+++ src/main/java/com/book/bookExample/BookExampleApplication.java
@@ -0,0 +1,12 @@
+package com.book.bookExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookExampleApplication.class, args);
+	}
+}
